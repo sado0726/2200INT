7aba04a8964756c0ad95f255b87a15ebeff838c0
Sado <dosadodobird0726@gmail.com>
2015-02-18 11:35:15 -0500

    last commit before implementation

--- Assembly/prj2.s
+++ Assembly/prj2.s
@@ -66,25 +66,25 @@ factorial:      addi    $sp, $sp, -1    ! push RA
                 beq     $a0, $s1, base_one
                 beq     $zero, $zero, recurse
                 
-    base_zero:  addi    $v0, $zero, 1   ! 0! = 1
+base_zero:	addi    $v0, $zero, 1   ! 0! = 1
                 beq     $zero, $zero, done
 
-    base_one:   addi    $v0, $zero, 1   ! 1! = 1
+base_one:	addi    $v0, $zero, 1   ! 1! = 1
                 beq     $zero, $zero, done
 
-    recurse:    add     $s1, $a0, $zero     ! save n in s1
+recurse:	add     $s1, $a0, $zero     ! save n in s1
                 addi    $a0, $a0, -1        ! n! = n * (n-1)!
                 la      $at, factorial
                 jalr    $at, $ra
 
                 add     $s0, $v0, $zero     ! use s0 to store (n-1)!
                 add     $v0, $zero, $zero   ! use v0 as sum register
-        mul:    beq     $s1, $zero, done    ! use s1 as counter (from n to 0)
+mul:		beq     $s1, $zero, done    ! use s1 as counter (from n to 0)
                 add     $v0, $v0, $s0
                 addi    $s1, $s1, -1
                 beq     $zero, $zero, mul
 
-    done:       lw      $s1, 0($sp)     ! pop s1
+done:		lw      $s1, 0($sp)     ! pop s1
                 addi    $sp, $sp, 1
                 lw      $s0, 0($sp)     ! pop s0
                 addi    $sp, $sp, 1
@@ -125,19 +125,19 @@ ti_inthandler:	addi	$sp, $sp, -1
 		addi	$a0, $a0, 1	!add to seconds
 		sw	$a0, 0($at)	!store back in to the memory
 		beq	$zero, $zero, end	
-min		sw	$zero 0($at)	!makes seconds back to zero
+min:		sw	$zero 0($at)	!makes seconds back to zero
 		lw	$a0, 1($at)	!gets value from minutes
 		beq	$a1, $a0, hr	!check if minutes is 59
 		addi	$a0, $a0, 1	!add to minutes
 		sw	$a0, 1($at)	!store back in to the memory
 		beq	$zero, $zero, end
-hr		sw	$zero 1($at)	!makes minutes back to zero
+hr:		sw	$zero 1($at)	!makes minutes back to zero
 		lw	$a0, 2($at)	!gets value from hours
 		addi	$a0, $a0, 1	!add to hours
 		sw	$a0, 2($at)	!store back in to the memory		
-				
+!end of interrupt timer			
 
-end		bonj	$ra, $sp	!restore all the registers
+end:		bonj	$ra, $sp	!restore all the registers
 		bonj	$fp, $sp
 		bonj	$sp, $sp
 		bonj	$k0, $sp
